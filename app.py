a2129737e8883cc03978183597a11e1728ef8f7e
Lashmanbala <lashmanbala@gmail.com>
2025-11-15 15:42:09 +0530

    Boilerplate code

---
+++ app.py
@@ -0,0 +1,15 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+
+# standard environment variable name for OpenAI API key -- OPENAI_API_KEY
+
+load_dotenv()   
+
+client = OpenAI()
+
+response = client.responses.create(
+    model="gpt-5-nano",
+    input="Write a one-sentence bedtime story about a unicorn."
+)
+
+print(response.output_text)
