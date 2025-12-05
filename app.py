fe47c88677a77722532b52a0036b682bf14204c9
Lashmanbala <lashmanbala@gmail.com>
2025-12-05 15:53:16 +0530

    Saving the content as file

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from openai import OpenAI
 from dotenv import load_dotenv
-from prompt import prompt
+from prompt import prompt, sys_msg
 from write_doc import auth_docs, build_requests_from_html
 import os
 import time
@@ -18,18 +18,18 @@ OUTPUT_DIR = "."
 MAX_TOKENS = 2500
 # max_completion_tokens = 2500
 
-USERNAME = os.getenv("WP_USERNAME")
-APP_PASSWORD = os.getenv("WP_APP_PASSWORD")
-WP_URL = os.getenv("WP_URL")
-featured_img_url = os.getenv("FEATURED_IMAGE_URL")
-social_image = os.getenv("SOCIAL_IMAGE_URL")
-country_name = os.getenv("COUNTRY_NAME")
-category_name = os.getenv("CATEGORY_NAME")
-page_title = os.getenv("page_title_format")
-key_phrase = os.getenv("key_phrase_format")
-description = os.getenv("description_format")
-brand_name = os.getenv("BRAND_NAME")
-print(WP_URL)
+# USERNAME = os.getenv("WP_USERNAME")
+# APP_PASSWORD = os.getenv("WP_APP_PASSWORD")
+# WP_URL = os.getenv("WP_URL")
+# featured_img_url = os.getenv("FEATURED_IMAGE_URL")
+# social_image = os.getenv("SOCIAL_IMAGE_URL")
+# country_name = os.getenv("COUNTRY_NAME")
+# category_name = os.getenv("CATEGORY_NAME")
+# page_title = os.getenv("page_title_format")
+# key_phrase = os.getenv("key_phrase_format")
+# description = os.getenv("description_format")
+# brand_name = os.getenv("BRAND_NAME")
+# print(WP_URL)
 
 '''
 docs_service = auth_docs()
@@ -57,36 +57,38 @@ print(tab_dict)
 cities = [tab["tabProperties"]["title"] for tab in tabs] '''
 # city_name = "Mumbai"
 
-cities = ["Delhi", "Bangalore"]
+cities = ["Chennai"]
 
 prompt_template = prompt
 
 for city_name in cities:
     s_time = time.time()
-    print(f"\n\n=== Generating content for city: {city_name} ===\n")
+    print(f"\n=== Generating content for city: {city_name} ===\n")
+    time.sleep(10)
     country_name = "India"
     # prompt = build_prompt(city)
-    # filename_safe = city_name.lower().replace(" ", "_")
-    # output_path = os.path.join(OUTPUT_DIR, f"{filename_safe}_seo_page2.txt")
+    filename_safe = city_name.lower().replace(" ", "_")
+    output_path = os.path.join(OUTPUT_DIR, f"{filename_safe}_seo_page3.txt")
 
     # Basic retry/backoff in case of transient failures
     max_retries = 1
     backoff = 5.0
 
     final_prompt = prompt_template.format(city_name=city_name, country_name=country_name)
-    
+     
     for attempt in range(1, max_retries + 1):
         try:
-            print(f"[Attempt {attempt}] Calling model {MODEL} to generate content for {city_name}...")
+            print(f"[Attempt {attempt}] Calling model to generate content for {city_name}...")
+            time.sleep(40)
             response = client.chat.completions.create(
                 model=MODEL,
                 messages=[
-                    {"role": "system", "content": "You are a 20+ years experienced SEO-friendly & user-friendly website content writer and a professional SEO copywriter who must follow formatting and content instructions exactly. Output the content strictly in html format. You are going to write content for multiple cities. Each content must be unique and the structure must be unique. "},
+                    {"role": "system", "content": sys_msg},
                     {"role": "user", "content": final_prompt}
                 ],
                 max_tokens=MAX_TOKENS,
                 # max_completion_tokens=MAX_TOKENS
-                temperature=0.5  # lower temperature for consistent output
+                temperature=0.5  
             )
             
             choices = response.choices or []  # Extract content
@@ -102,21 +104,21 @@ for city_name in cities:
             else:
                 print('----Content generated----')
                 e_time = time.time()
-                print(e_time)
-                print(e_time - s_time)
+                # print(e_time - s_time)
                 usage = response.usage or {}
                 print(f'Usage: {usage}')
             
-            # with open(output_path, "w", encoding="utf-8") as f:
-            #     f.write(content)
-            #     print('++++written to file++++')
+            with open(output_path, "w", encoding="utf-8") as f:
+                f.write(content)
+                print('++++written to file++++')
+                time.sleep(10)
             
             # tab_id = tab_dict[city_name]
             # requests = build_requests_from_html(content, tab_id=tab_id)
  
             # if not requests:
             #     print("No requests generated.")
-                # return
+            #     return
 
             # Send batchUpdate
             # batch = {"requests": requests}
@@ -128,11 +130,11 @@ for city_name in cities:
 
             # print(f"✅ Generated content saved to: {output_path}")
 
-            response = post_to_wp(content, featured_img_url, page_title, brand_name, key_phrase, description, social_image, WP_URL, USERNAME, APP_PASSWORD)
+            # response = post_to_wp(content, featured_img_url, page_title, brand_name, key_phrase, description, social_image, WP_URL, USERNAME, APP_PASSWORD)
             # print(f"✅ Generated content posted to WordPress for city: {city_name}")
-            if response.status_code == 201:
-                page_url = response.json().get("link", "")
-                print(f"✅ Created page for '{city_name}': {page_url}")
+            # if response.status_code == 201:
+            #     page_url = response.json().get("link", "")
+            #     print(f"✅ Created page for '{city_name}': {page_url}")
 
         except Exception as e:
             print(f"Error on attempt {attempt}: {e}")
@@ -140,5 +142,3 @@ for city_name in cities:
                 raise
             else:
                 time.sleep(backoff * attempt)
-
-
